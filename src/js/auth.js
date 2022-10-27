99d2e62a84d03abc000b302fbabbea7cf9fc9b9a
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-10-28 01:03:07 +0300

    start

---
+++ src/js/auth.js
@@ -0,0 +1,25 @@
+import { refs } from './refs';
+/* import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDRjTFbmRann_cr3ckzqO-WJzrN46pmMNY',
+  authDomain: 'ultimate-choir-311114.firebaseapp.com',
+  projectId: 'ultimate-choir-311114',
+  storageBucket: 'ultimate-choir-311114.appspot.com',
+  messagingSenderId: '747352375066',
+  appId: '1:747352375066:web:bfd52aa1c7f0703dcff6a3',
+  measurementId: 'G-XPVPECFXHK',
+}; */
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+function closeModal() {
+  refs.bckDrop.classList.add('is-hidden');
+}
+
+refs.signBtn.addEventListener('click', () => {
+  refs.bckDrop.classList.remove('is-hidden');
+});
+refs.closeBtn.addEventListener('click', closeModal);
---
+++ src/js/refs.js
@@ -0,0 +1,8 @@
+export const refs = {
+  signBtn: document.querySelector('button[data-sign="SignIn"]'),
+  closeBtn: document.querySelector('button[data-modal-close]'),
+  bckDrop: document.querySelector('div[data-backdrop="backdrop"]'),
+  logBtn: document.querySelector('button[data-log="log"]'),
+  inputEmail: document.querySelector('[data-email="email"]'),
+  inputPass: document.querySelector('[data-password="password"]'),
+};
