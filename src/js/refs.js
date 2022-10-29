3645a5070547b807a05c2e9b02fae56a85c8a9ca
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-10-29 04:07:33 +0300

    wqeq

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,8 +1,12 @@
 export const refs = {
-  signBtn: document.querySelector('button[data-sign="SignIn"]'),
+  signBtn: document.querySelector('button[data-opensign]'),
+  signOutBtn: document.querySelector('button[data-signout]'),
   closeBtn: document.querySelector('button[data-modal-close]'),
-  bckDrop: document.querySelector('div[data-backdrop="backdrop"]'),
-  logBtn: document.querySelector('button[data-log="log"]'),
-  inputEmail: document.querySelector('[data-email="email"]'),
-  inputPass: document.querySelector('[data-password="password"]'),
+  bckDrop: document.querySelector('div[data-backdrop]'),
+  signInBtn: document.querySelector('button[data-signin]'),
+  signUpBtn: document.querySelector('button[data-signup]'),
+  inputEmail: document.querySelector('[data-email]'),
+  inputPass: document.querySelector('[data-password]'),
+  googleBtn: document.querySelector('[data-google]'),
+  faseBtn: document.querySelector('[data-fasebook]'),
 };
